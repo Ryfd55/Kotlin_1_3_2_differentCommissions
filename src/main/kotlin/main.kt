ca642adbee9beb8ba7e53095af5da183d785274d
Denis Gubardin <gubardin1984@mail.ru>
2023-01-15 15:36:48 +0400

    finished prog

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,38 +1,48 @@
+import kotlin.math.max
+
+const val cardMonthLimit = 600000
+const val vkMonthLimit = 40000
+const val vkMaxTransfer = 15000
+const val mcCommissionStart = 75000
+const val varCommVisa = 0.0075
+const val minCommVisa = 35
+const val varCommMC = 0.006
+const val statCommMC = 20
+
 fun main() {
-    val res = userCardType("Visa", 100000)
-    print("Комиссия составляет $res копеек")
+    val res = userCardType(100000, "MasterCard", 20000)
+    if (res != -1) {
+        println("Перевод успешно проведен.")
+        println("Комиссия за перевод составляет $res руб.")
+    } else {
+        println("Перевод не проведен")
+        println("Превышен лимит")
+    }
 }
 
-
-fun userCardType(card: String = "VK Pay", transferAmmount: Int = 0): Double {
+fun userCardType(transAmmount: Int, card: String = "VK Pay", monthTrasfered: Int = 0): Int {
     return when (card) {
-        "MasterCard" -> getMCCommission(transferAmmount)
-        "Visa", "Mir" -> getVisaMirCommission(transferAmmount)
+        "VK Pay" -> if ((monthTrasfered + transAmmount > vkMonthLimit) || transAmmount > vkMaxTransfer)
+            -1 else 0
         else -> {
-            getVKCommission(transferAmmount)
+            if (monthTrasfered + transAmmount > cardMonthLimit) {
+                -1
+            } else {
+                when (card) {
+                    "MasterCard" -> {
+                        getMCCommission(transAmmount, monthTrasfered)
+                    }
+                    else -> {
+                        getVisaMirCommission(transAmmount)
+                    }
+                }
+            }
         }
     }
 }
 
-fun getMCCommission(transferAmmount: Int): Double {
-    var commission = 0.0
-    if (transferAmmount > 75000) {
-        commission = (transferAmmount * 0.006 + 20)
-    }
-    return commission
-}
+fun getMCCommission(transAmmount: Int, monthTrasfered: Int) = if (transAmmount + monthTrasfered > mcCommissionStart)
+    ((transAmmount + monthTrasfered - mcCommissionStart) * varCommMC + statCommMC).toInt() else 0
 
-fun getVisaMirCommission(transferAmmount: Int): Double {
-    var commission = 0.0
-    if (transferAmmount*0.0075 < 35) {
-        commission = 35.00}
-    else{
-        commission = (transferAmmount * 0.0075)
-    }
-    return commission
-}
+fun getVisaMirCommission(transferAmmount: Int) = max((transferAmmount * varCommVisa).toInt(), minCommVisa)
 
-fun getVKCommission(transferAmmount: Int): Double {
-    var commission = 0.0
-    return commission
-}
